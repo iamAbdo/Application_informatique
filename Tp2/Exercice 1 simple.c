4b42c4238395ace9c56e11e2b61c217bb5df6887
iamAbdo <75045826+iamAbdo@users.noreply.github.com>
2023-10-18 22:17:27 +0100

    Create Exercice 1 simple.c

---
+++ Tp2/Exercice 1 simple.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,d,n,Un;
+    printf("donner a\n");
+    scanf("%d",&a);
+    printf("donner d\n");
+    scanf("%d",&d);
+    printf("donner n\n");
+    scanf("%d",&n);
+    Un = a + n*d;
+    printf("Un = ")
+
+    return 0;
+}
